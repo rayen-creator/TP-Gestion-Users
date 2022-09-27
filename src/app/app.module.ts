c76abf7086ae92438c87ae4e118b57e33cf31968
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-09-27 09:50:55 +0200

    session 3 code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListProductComponent } from './list-product/list-product.component';
 import { FooterComponent } from './footer/footer.component';
+import { UserListComponent } from './user-list/user-list.component';
 /*.In our AppModule, we then need to specify the component that will serve as the entry point component for our application.
 In our app.module.ts file where we import the entry component (conventionally AppComponent) and supply it as the only item
 in our bootstrap array inside the NgModule configuration object. eg. bootstrap[AppComponent]*/
@@ -13,7 +14,8 @@ in our bootstrap array inside the NgModule configuration object. eg. bootstrap[A
     AppComponent,
     HeaderComponent,
     ListProductComponent,
-    FooterComponent
+    FooterComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-product.component.css']
 })
 export class ListProductComponent implements OnInit {
-
-  constructor() { }
-
+  public title: String;
+  constructor() {
+  }
   ngOnInit(): void {
+    this.title = 'MyStore App'
   }
 
 }
